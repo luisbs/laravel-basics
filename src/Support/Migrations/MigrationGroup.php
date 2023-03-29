e4d711093ac84c4f7b73862651c5851368c785d1
Luis Brayan <luis.bs.9004@gmail.com>
2023-03-29 10:40:41 -0600

    feat: added feedback messages

--- src/Support/Migrations/MigrationGroup.php
+++ src/Support/Migrations/MigrationGroup.php
@@ -3,6 +3,7 @@
 namespace Basics\Support\Migrations;
 
 use Illuminate\Database\Migrations\Migration;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
 /**
  * Executes a group of migrations together.
@@ -23,8 +24,11 @@ class MigrationGroup extends Migration
      */
     public function up()
     {
+        $output = new ConsoleOutput();
         foreach ($this->migrations as $cls) {
+            $output->writeln("Running {$cls}->up()");
             (new $cls())->up();
+            $output->writeln("Done {$cls}->up()");
         }
     }
 
@@ -36,8 +40,11 @@ class MigrationGroup extends Migration
     public function down()
     {
         // runs on reverse to prevent order problems
+        $output = new ConsoleOutput();
         foreach (array_reverse($this->migrations) as $cls) {
+            $output->writeln("Running {$cls}->down()");
             (new $cls())->down();
+            $output->writeln("Done {$cls}->down()");
         }
     }
 }
